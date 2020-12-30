2cbb41db6679db482b057a2aab55a6296309c76d
Greg Foster <gregorymfoster@gmail.com>
2020-12-30 16:05:11 -0500

    Automated Release

--- Formula/screenplay.rb
+++ Formula/screenplay.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Screenplay < Formula
   desc "The Screenplay CLI allows you to add Screenplay (https://screenplay.dev) to existing xcode projects."
   homepage "https://github.com/screenplaydev/screenplay-cli/"
-  url "https://github.com/screenplaydev/screenplay-cli/archive/v0.14.1.zip"
-  sha256 "2e2de7f12e02f148a7dbaf3c7a9ffae1dc4c19ae2e8d2b73e769f9fee35114e7"
+  url "https://github.com/screenplaydev/screenplay-cli/archive/v0.15.0.zip"
+  sha256 "29e56b82b9aed5be9b21bf4910e0b82d87a0a69d3ec1af1f499e9388db919c26"
   license "None"
 
   depends_on "node"
