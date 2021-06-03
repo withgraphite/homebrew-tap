ffbc846a7b56282e27c190b61c49c6be218c9458
Nicholas Yan <nicholasyan11@gmail.com>
2021-06-02 20:23:50 -0700

    Automated Release

--- Formula/screenplay.rb
+++ Formula/screenplay.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Screenplay < Formula
   desc "The Screenplay CLI allows you to add Screenplay (https://screenplay.dev) to existing xcode projects."
   homepage "https://github.com/screenplaydev/screenplay-cli/"
-  url "https://github.com/screenplaydev/screenplay-cli/archive/v0.35.4.zip"
-  sha256 "49c8bde4e44b381f4940072a398fcb5698dd16ddfa7982ee095cb4719a496c78"
+  url "https://github.com/screenplaydev/screenplay-cli/archive/v0.35.5.zip"
+  sha256 "2262e9690246ee38d2c36f7bdd7457589f6091431ae4d6ad0e00a8d1a3459980"
   license "None"
 
   depends_on "node"
@@ -14,7 +14,6 @@ class Screenplay < Formula
     system "npm", "install", "--prefix", "#{libexec}/lib/node_modules/screenplay-cli/screenplay-libs/retype", *Language::Node.local_npm_install_args
     system "npm", "install", "--prefix", "#{libexec}/lib/node_modules/screenplay-cli/screenplay-libs/shared_routes", *Language::Node.local_npm_install_args
     system "npm", "install", "--prefix", "#{libexec}/lib/node_modules/screenplay-cli/screenplay-libs/xcodejs", *Language::Node.local_npm_install_args
-    system "npm", "install", "--prefix", "#{libexec}/lib/node_modules/screenplay-cli/screenplay-libs/splog", *Language::Node.local_npm_install_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
