c2458e058665113bd96abfad01fadd0a42e4789a
Nicholas Yan <nicholasyan11@gmail.com>
2021-07-18 18:32:46 -0400

    Automated Release

--- templates/graphite.rb
+++ templates/graphite.rb
@@ -11,6 +11,7 @@ class Graphite < Formula
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
