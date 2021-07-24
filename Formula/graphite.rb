779f719b73a9de4b7ae4b55d14b50080ff9fa078
Greg Foster <gregorymfoster@gmail.com>
2021-07-24 17:07:10 -0400

    Automated Release

--- Formula/graphite.rb
+++ Formula/graphite.rb
@@ -4,7 +4,7 @@ class Graphite < Formula
   desc "The Graphite CLI allows you to easily manage your stacked-diff workflow."
   homepage "https://graphite.dev/"
   url "https://github.com/screenplaydev/graphite-cli/archive/v0.3.1.zip"
-  sha256 "a5545e4d11e86ec6024495dfdf4d949aabfdd4cb9a59dfc62096aaf9529ea27b"
+  sha256 "1afac9151aa4eef49e23a76d90b996e2a245cec205cf64b08b870a66e5f97020"
   license "None"
 
   depends_on "node"
