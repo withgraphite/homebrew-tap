91161281c11d8c5072de87bec27e3cee92134adc
Nicholas Yan <nicholasyan11@gmail.com>
2021-09-16 20:17:59 -0400

    Automated Release

--- Formula/graphite.rb
+++ Formula/graphite.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Graphite < Formula
   desc "The Graphite CLI allows you to easily manage your stacked-diff workflow."
   homepage "https://graphite.dev/"
-  url "https://github.com/screenplaydev/graphite-cli/archive/v0.13.1.zip"
-  sha256 "1103c97ff46a2ef63d6641cc1682dac9cd87104ee7c5a65f8690939c3e1b446a"
+  url "https://github.com/screenplaydev/graphite-cli/archive/v0.14.0.zip"
+  sha256 "09df269830f0f946e962101fded1696d6e45b50ddc605e96cb37581d68a5a7bb"
   license "None"
 
   depends_on "node"
