bbbebe58203878a300e11615a3db37de3754f881
Greg Foster <greg@graphite.dev>
2022-05-05 22:08:55 +0000

    Automated release: v0.18.01651788523-nightly

--- Formula/graphite-nightly.rb
+++ Formula/graphite-nightly.rb
@@ -3,9 +3,9 @@ require "language/node"
 class GraphiteNightly < Formula
   desc "The Graphite CLI allows you to easily manage your stacked-diff workflow."
   homepage "https://graphite.dev/"
-  url "https://github.com/screenplaydev/graphite-cli/archive/v0.18.01651702096-nightly.zip"
-  sha256 "10ab8947729908430c616a65e282c2016c4ca261b64f969d26cacef6edd9ed17"
-  version "0.18.01651702096"
+  url "https://github.com/screenplaydev/graphite-cli/archive/v0.18.01651788523-nightly.zip"
+  sha256 "4e682bc7feb5dedc9eeb5c23d0f09b02b017fdc68044e9fcffd526ba79f15607"
+  version "0.18.01651788523"
   license "None"
 
   depends_on "node"
