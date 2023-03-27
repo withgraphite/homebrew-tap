2135739e423dd22e7984924451f23fd3f4f8640e
Jacob Gold <jacob@graphite.dev>
2023-03-27 11:46:39 -0400

    try fix

--- Formula/graphite-exe-experimental.rb
+++ Formula/graphite-exe-experimental.rb
@@ -7,11 +7,17 @@ class GraphiteExeExperimental < Formula
   if OS.mac?
     url "https://github.com/withgraphite/graphite-cli/releases/download/pkg-exp/graphite-cli-macos"
     sha256 "7ddddd7ba9856eaaa64e1480f91be33afed1bfafdc0078ca5b11dd36b59746f4"
+    def install
+      bin.install "graphite-cli-macos" => "gt"
+    end  
   end
 
   if OS.linux?
     url "https://github.com/withgraphite/graphite-cli/releases/download/pkg-exp/graphite-cli-linux"
     sha256 "95148e245837dc6fdb25fa086bde7893671110865ddd2db9c41952c7ba60b1a7"
+    def install
+      bin.install "graphite-cli-linux" => "gt"
+    end  
   end
 
   # TODO
@@ -19,9 +25,6 @@ class GraphiteExeExperimental < Formula
   # Hardware::CPU.arm?
   # Hardware::CPU.is_64_bit?
 
-  def install
-    bin.install "gt"
-  end
 
   test do
     raise "Test not implemented."
