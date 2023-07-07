e8e726487c8786980caf3d4728a4e84c05ce51a4
David Balatero <dbalatero@users.noreply.github.com>
2023-07-07 11:44:21 -0400

    Release v0.20.22

--- Formula/graphite.rb
+++ Formula/graphite.rb
@@ -1,20 +1,20 @@
 class Graphite < Formula
   desc "The Graphite CLI allows you to easily manage your stacked-diff workflow."
   homepage "https://graphite.dev/"
-  version "0.20.21"
+  version "0.20.22"
   license "None"
 
   if OS.mac?
-    url "https://github.com/withgraphite/graphite-cli/releases/download/v0.20.21/gt-macos"
-    sha256 "7d4f2dd797e105cc1d801ad5ab84d0faaa43abf434cb40997f68f198d9eb337d"
+    url "https://github.com/withgraphite/graphite-cli/releases/download/v0.20.22/gt-macos"
+    sha256 "e522da7fc73a4339061c2cc5898c91e733cfe9af73836b0c6e9b8317dd35c511"
     def install
       bin.install "gt-macos" => "gt"
     end  
   end
 
   if OS.linux?
-    url "https://github.com/withgraphite/graphite-cli/releases/download/v0.20.21/gt-linux"
-    sha256 "14f66826a75728e7e09f00e41fbc4f4c11395eb03ca5cdbe34789c065d640c10"
+    url "https://github.com/withgraphite/graphite-cli/releases/download/v0.20.22/gt-linux"
+    sha256 "bf451c37cd8d05759010f3d9eb7d6472e36cf5ed5595cdf6b9f7eb2192aaca1b"
     def install
       bin.install "gt-linux" => "gt"
     end  
