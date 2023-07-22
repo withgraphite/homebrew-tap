3a9648f7589c2ec9bcd1544d41e1fab19c3efe50
Jacob Gold <jacob@graphite.dev>
2023-07-22 16:38:11 -0400

    test

--- Formula/graphite.rb
+++ Formula/graphite.rb
@@ -1,20 +1,20 @@
 class Graphite < Formula
-  desc "The Graphite CLI allows you to easily manage your stacked pull changes and their pull requests."
+  desc "The Graphite CLI allows you to easily manage your stacked-diff workflow."
   homepage "https://graphite.dev/"
-  version "0.20.34"
+  version "0.21.0"
   license "None"
 
   if OS.mac?
-    url "https://github.com/withgraphite/graphite-cli/releases/download/v0.20.34/gt-macos"
-    sha256 "acf6a53b873703fcaa190293aae33a12a5db17e0bb21c3c3713e37d771c86de1"
+    url "https://github.com/withgraphite/homebrew-tap/releases/download/v0.21.0/gt-macos"
+    sha256 "b498330dbfea3810ec0822d4d106f69fe8182c9a0eefbb8b9b794500c22cc573"
     def install
       bin.install "gt-macos" => "gt"
     end  
   end
 
   if OS.linux?
-    url "https://github.com/withgraphite/graphite-cli/releases/download/v0.20.34/gt-linux"
-    sha256 "61c19e9ceb2fe884b1f4f6eebaf96746ef048958c4f382a3c4dc82d6f1a95851"
+    url "https://github.com/withgraphite/homebrew-tap/releases/download/v0.21.0/gt-linux"
+    sha256 "ea22d355da93c59300ee83ac9ef58d63126c41a80d9a39076a5de9420ae789d9"
     def install
       bin.install "gt-linux" => "gt"
     end  
