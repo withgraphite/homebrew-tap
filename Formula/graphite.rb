57437fb4be321579bf45441b7482ffc03cbd463f
Production Assistant <79435273+production-assistant@users.noreply.github.com>
2023-11-27 17:42:03 +0000

    Release v1.0.10 to stable

--- Formula/graphite.rb
+++ Formula/graphite.rb
@@ -2,18 +2,18 @@ class Graphite < Formula
   desc "Allows you to manage your stacked changes and submit them for review on GitHub"
   homepage "https://graphite.dev/"
   license "AGPL-3.0"
-  version "1.0.9"
+  version "1.0.10"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/withgraphite/homebrew-tap/releases/download/v1.0.9/gt-macos-arm64"
-      sha256 "bf22e95b42bae211d2a70dd1dc69e6fd5a811620baa125c80c3e171738ed4b1a"
+      url "https://github.com/withgraphite/homebrew-tap/releases/download/v1.0.10/gt-macos-arm64"
+      sha256 "f2e620a4b25b9bb51c6cefac7d16b584f3510a01f2983c35876666d4ddfa7bed"
       def install
         bin.install "gt-macos-arm64" => "gt"
       end
     else
-      url "https://github.com/withgraphite/homebrew-tap/releases/download/v1.0.9/gt-macos-x64"
-      sha256 "8b5632421d736fa30a295eb330a502e8315784c36dfb0ddc16d634b08faf3548"
+      url "https://github.com/withgraphite/homebrew-tap/releases/download/v1.0.10/gt-macos-x64"
+      sha256 "280438d3476056ee7f10d29b5a6b9f1db1a982267f5f6ec30254977404a0ab6a"
       def install
         bin.install "gt-macos-x64" => "gt"
       end
@@ -21,8 +21,8 @@ class Graphite < Formula
   end
 
   if OS.linux?
-    url "https://github.com/withgraphite/homebrew-tap/releases/download/v1.0.9/gt-linux"
-    sha256 "ccc6cbf8a79e5227948b8666044b648025ae4dcec5ee6ac92d8a5c065fa495f2"
+    url "https://github.com/withgraphite/homebrew-tap/releases/download/v1.0.10/gt-linux"
+    sha256 "2f30b5d196552bd72000b258f9dc06260f21af1db8adfa7b14638fb68cead47b"
     def install
       bin.install "gt-linux" => "gt"
     end
